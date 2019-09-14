baa26e5f8386368df65693cc1c984c9891e4011e
Sujata Regoti <in-sujatar@Sujatas-MacBook-Pro.local>
2019-09-14 10:20:32 +0530

    Add list of name and show names list

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TextInput, View, Button, Text } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [names, setNames] = useState([]);
+
+  const addName = () => {
+    setNames(names => [...names, name]);
+    setName('');
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="name"
-          style={{ borderBottomColor: "black", flex:1, margin: 10, borderBottomWidth: 1 }}
+          style={styles.input}
+          onChangeText={setName}
+          value={name}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addName} />
+      </View>
+      <View>
+        {names.map(name => (
+          <Text key={name}>{name}</Text>
+        ))}
       </View>
     </View>
   );
@@ -17,10 +32,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  input: {
+    borderBottomColor: "black",
+    flex: 1,
+    margin: 10,
+    borderBottomWidth: 1
   }
 });
